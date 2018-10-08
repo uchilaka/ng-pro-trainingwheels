61884cf1c256b62d16c9eb2e4ec5558d4c04c0e3
Uchenna Chilaka <uchenna.chilaka@alliancedata.com>
2018-10-08 17:16:01 -0400

    first commit

---
+++ app/app.component.ts
@@ -0,0 +1,34 @@
+import { Component } from "@angular/core";
+
+import { User } from "./auth-form/auth-form.interface";
+
+@Component({
+  selector: "app-root",
+  template: `
+    <div>
+      <auth-form
+        (submitted)="createUser($event)">
+        <h3>Create account</h3>
+        <button type="submit">
+          Register
+        </button>
+      </auth-form>
+      <auth-form
+        (submitted)="loginUser($event)">
+        <h3>Login</h3>
+        <button type="submit">
+          Submit
+        </button>
+      </auth-form>
+    </div>
+  `
+})
+export class AppComponent {
+  createUser(user: User): void {
+    console.log("Create account", user);
+  }
+
+  loginUser(user: User): void {
+    console.log("Login", user);
+  }
+}
